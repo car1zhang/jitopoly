aa5252548eaec1111e79827fcce4d2b2aa92b1e2
Kyan <kyanliu9@gmail.com>
2024-12-08 01:27:12 -0500

    basic game map functions

--- client/src/map.ts
+++ client/src/map.ts
@@ -11,7 +11,8 @@ interface PropertyTile {
   basePrice: number;
   upgradePrice: number;
   houses: number;
-  owner?: Player;
+  rent: number[]; // different rent prices, consumes 6 numbers
+  owner?: Player; // if houses>0, owner? exists assume if house is 1 the property is owned
 }
 
 interface EventTile {
@@ -29,8 +30,23 @@ class GameMap { // list of tiles must be divisible by 4
     this.countries = countries;
   }
 
+  public mapSize = (): number => {
+    return this.tiles.length;
+  }
+  public getTile = (indx:number): Tile => {
+    return this.tiles[indx];
+  }
+  public updateTiles = (newTile:Tile): void => {
+    for(let i=0;i<this.tiles.length;i++){
+      if(this.tiles[i].name == newTile.name){
+        this.tiles[i]=newTile;
+        return;
+      }
+    }
+  }
+
   private tiles: Tile[] = [];
-  private countries: Country[] = [];
+  private countries: Country[] = []; // contains addresses to png?
 }
 
 export { GameMap };
